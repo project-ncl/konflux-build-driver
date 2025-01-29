1308fb922fcb459e39cf797a2714b2119104c4f0
Nick Cross <ncross@redhat.com>
2025-01-29 14:20:49 +0000

    Add domain proxy and indy params. Add logging

--- src/main/java/org/jboss/pnc/konfluxbuilddriver/util/Configuration.java
+++ src/main/java/org/jboss/pnc/konfluxbuilddriver/util/Configuration.java
@@ -1,5 +1,7 @@
 package org.jboss.pnc.konfluxbuilddriver.util;
 
+import java.util.Optional;
+
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithName;
 
@@ -14,9 +16,19 @@ public interface Configuration {
     @WithName("pipeline-resolver")
     String resolverTarget();
 
+    String domainProxyAllowList();
+
+    String domainProxyInternalNonProxyHosts();
+
     String indyProxyEnabled();
 
     String selfBaseUrl();
 
     Boolean notificationEnabled();
+
+    // Currently defaults to empty string but according to
+    // https://github.com/eclipse/microprofile-config/issues/446 we need to use an Optional.
+    Optional<String> indyProxyClientID();
+
+    Optional<String> indyProxyClientCredential();
 }
